7aa37e1c01098d904693c90a26abed2aeebd62fc
Benjamin Poile <benp51@outlook.com>
2018-01-26 10:12:54 -0800

    pushing for laptop development

--- PA2/PA2/Functions.c
+++ PA2/PA2/Functions.c
@@ -10,7 +10,8 @@
 
 
 int displayMenu() {
-	int menu = -1;
+	int menu = -1, submenu = -1;
+	char *query;
 	printf("\n(1)\tload\n(2)\tstore\n(3)\tdisplay\n(4)\tinsert\n(5)\tdelete\n(6)\tedit\n(7)\tsort\n(8)\trate\n(9)\tplay\n(10)\tshuffle\n(11)\texit");
 	
 	while (!(menu >= 1 && menu <= 11)) {
@@ -20,7 +21,131 @@ int displayMenu() {
 
 	printf("Success!");
 
-	if (menu == 11)
-		return 11;
+	switch (menu) {
+		case 3:
+			printf("\n(1)\tLoad all\n(2)\tLoad by Artist\n");
+			
+			while (!(submenu == 1 || submenu == 2)) {
+				printf("\n->\t");
+				scanf("%d", &submenu);
+			}
+
+			switch (submenu) {
+				case 1:
+					printList(pList);
+					break;
+				case 2:
+					printf("Artist?\n->\t");
+					scanf("%s", &query);
+					printList(pList, query);
+			}
+
+			break;
+		case 8:
+			printf("\nSong?\n->\t");
+			scanf("%s", &query);
+			rate(pList, query);
+			break;
+		case 11:
+			return 11;
+	}
+
 }
 
+Node * makeNode(Record newData) {
+	Node * pMem = NULL;
+
+	pMem = (Node *)malloc(sizeof(Node));
+
+	if (pMem != NULL) {
+		pMem->pNext = NULL;
+		pMem->data = newData;
+	}
+
+	return pMem;
+}
+
+Boolean insertFront(Node **pList, Record newData) {
+	Node *pMem = makeNode(newData);
+
+	if (pMem == NULL)
+		return (Boolean)FALSE;
+
+	pMem->pNext = *pList;
+	*pList = pMem;
+
+	return TRUE;
+}
+
+Boolean deleteContact(Node **pList, Record search) {
+	Node *pCurrent = *pList;
+
+	if (strcmp(pCurrent->data.song, search.song) == 0) {
+		pCurrent->pNext = pCurrent->pNext->pNext;
+		free(pCurrent->pNext);
+		return TRUE;
+	}
+
+	while ((pCurrent->pNext != NULL) && (strcmp(pCurrent->pNext->data.song, search.song) != 0))
+		pCurrent = pCurrent->pNext;
+
+	if (pCurrent->pNext != NULL) {
+		pCurrent->pNext = pCurrent->pNext->pNext;
+		free(pCurrent->pNext);
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+Boolean rate(Node *pList, char *query) {
+	Node *pCurrent = pList;
+
+	if (strcmp(pCurrent->data.song, query) == 0) {
+		pCurrent->data.rating = rate_query(); //where are ratings stored, object-wise????
+		return TRUE;
+	}
+
+	while ((pCurrent->pNext != NULL) && (strcmp(pCurrent->data.song, query) != 0))
+		pCurrent = pCurrent->pNext;
+
+	if (pCurrent->pNext != NULL) {
+		pCurrent->data.rating = rate_query();
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+// I wrote this because I'm lazy and I'm calling it twice in `rate()` so it seemed efficient
+int rate_query() { 
+	int newRating = -1;
+	
+	while ( !(newRating <= 5 && newRating >= 1) ) {
+		printf("Rating?\n->\t");
+		scanf("%s", &newRating);
+	}
+	
+	return newRating;
+}
+
+void printList(Node *pList) {
+	Node *pCurrent = pList;
+	
+	while (pCurrent->pNext != NULL) {
+		printf("\"%s\" \tby %s\n",pCurrent->data.song, pCurrent->data.artist);
+		pCurrent = pCurrent->pNext;
+	}
+}
+
+void printList(Node *pList, char *query) {
+	Node *pCurrent = pList;
+
+	printf("Music by %s:\n", query);
+	
+	while (pCurrent->pNext != NULL) {
+		if (strcmp(pCurrent->data.artist, query) == 0)
+			printf("\"%s\"", pCurrent->data.song);
+		pCurrent = pCurrent->pNext;
+	}
+}
--- PA2/PA2/Header.h
+++ PA2/PA2/Header.h
@@ -11,4 +11,36 @@
 #include <string.h> 
 
 
+//NOTE: I used code I wrote in lab with my group. Similarities in my Linked List code may be seen because of that.
+typedef enum boolean {
+	FALSE, TRUE
+} Boolean;
+
+typedef struct duration {
+	int minutes;
+	int seconds;
+} Duration;
+
+typedef struct record {
+	char *artist;
+	char *album;
+	char *song;
+	char *genre;
+	Duration length;
+	int plays;
+	int rating;
+} Record;
+
+typedef struct node {
+	Record data;	
+	struct node *pNext;
+	struct node *pPrev;
+} Node;
+
 int displayMenu();
+Node * makeNode(Record newData);
+Boolean insertFront(Node **pList, Record newData);
+Boolean deleteContact(Node **pList, Record search);
+Boolean rate(Node *pList, char *query);
+void printList(Node *pList);
+void printList(Node *pList, Record search);
